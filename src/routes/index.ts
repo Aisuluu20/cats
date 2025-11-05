f1c4f418f4aa204d7f13c5bc1a5c434686ba746e
Aisuluu <aisuluu@MacBook-Air-Aisuluu.local>
2025-11-05 10:55:41 +0600

    updated commit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,21 +1,28 @@
 import cors from "cors";
-import { Router } from "express";
+import express, { Router } from "express";
 import authRoutes from "../modules/auth/auth.routes";
 import catRoutes from "../modules/cat/cat.routes";
 const globalRouter = Router();
+
+const server = express();
 
 const corsConfig = {
   origin: [
     "http://localhost:5001",
     "http://localhost:3000",
-    "https://github.com/Aisuluu20/catback",
-    "https://catback.onrender.com",
+    "https://cat-store-szm4.onrender.com",
+    "https://dashboard.render.com/web/srv-d44tggi4d50c73epptvg",
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
 
-globalRouter.use("/cats", cors(corsConfig), catRoutes);
-globalRouter.use("/auth", cors(corsConfig), authRoutes);
+server.use(cors(corsConfig));
+server.options("*", cors(corsConfig));
+
+server.use(express.json());
+
+globalRouter.use("/cats", catRoutes);
+globalRouter.use("/auth", authRoutes);
 
 export default globalRouter;
